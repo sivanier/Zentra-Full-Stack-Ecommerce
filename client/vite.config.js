5b2915afc1e7cbff458527413fe02b1b5627688f
Sivanier <sivanier4@gmail.com>
2026-06-25 17:19:01 +0530

    Update API URL for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,2 +1,7 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins:[react()], base: process.env.GITHUB_ACTIONS ? '/Zentra/' : '/' });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Zentra-Full-Stack-Ecommerce/'
+})
